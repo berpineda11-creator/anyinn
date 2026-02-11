62216b480cc3f345d451565abbbb8411321ec444
berpineda11-creator <berpineda11@gmail.com>
2026-02-12 00:10:26 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const messages = [
-    "Are you sure?",
+    "Are you sure bebe?",
     "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
+    "Are you sure na talaga?",
+    "Bebe please...",
     "Just think about it!",
-    "If you say no, I will be really sad...",
+    "If you say no, I will be really sad bebe...",
     "I will be very sad...",
     "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "Ok fine, I will stop asking na...",
+    "Just kidding, say yes please my tortol! ❤️"
 ];
 
 let messageIndex = 0;
